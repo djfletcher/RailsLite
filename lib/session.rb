963b6cb1d5cebf61c123b848f562d6ce5b3e7106
daniel <daniel@daniels-MacBook-Pro-2.local>
2017-02-21 17:11:06 -0800

    Implemented Session class to store and retrieve cookies in web browsers

--- lib/session.rb
+++ lib/session.rb
@@ -1,19 +1,29 @@
 require 'json'
+require 'byebug'
 
 class Session
   # find the cookie for this app
   # deserialize the cookie into a hash
   def initialize(req)
+    @cookies = find_and_deserialize_cookies(req)
+  end
+
+  def find_and_deserialize_cookies(req)
+    cookies = req.cookies['_rails_lite_app']
+    cookies.nil? ? {} : JSON.parse(cookies)
   end
 
   def [](key)
+    @cookies[key]
   end
 
   def []=(key, val)
+    @cookies[key] = val
   end
 
   # serialize the hash into json and save in a cookie
   # add to the responses cookies
   def store_session(res)
+    res.set_cookie('_rails_lite_app', path: '/', value: @cookies.to_json)
   end
 end
